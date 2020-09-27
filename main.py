9d4cd2f07204f15d442bf3a7bb5fd8a7746b086a
Ernest Cheung <ErnestCheung02@gmail.com>
2020-09-27 13:25:21 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+def get_file_lines(filename):
+    return poem
+
+def lines_printed_backwards(lines_list):
+    print()
+
+
+def lines_printed_random(lines_list):
+    print()
+
+def lines_printed_custom(lines_list):
+
+
+
+
