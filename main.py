0287b14c9db5eb78b67f54617c2846b79df060ce
Ernest Cheung <ErnestCheung02@gmail.com>
2020-10-01 19:30:52 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,54 @@
-input_file_name =  'poem.txt'
-infile = open(input_file_name, "r")
 
-
-
-def reverse_list_print():
-    outfile.reverse()
-
-def reverse_list():
-    with open(input_file_name, "r") as outfile:
-        outfile.reverse() 
-    return reverse_list_print()
 
 def get_file_lines(filename):
-    return reverse_list
+    inline = open('poem.txt', 'r')
+    lines = inline.readlines()
+    poem = inline.read()
+    inline.close()
+    return lines
+
+print(get_file_lines('poem.txt'))
+poems = get_file_lines('poem.txt')
+
+def lines_printed_backwards(lines_list):
+    line = len(poems)
+    number = 0
+    for i in poems
+        if number < line:
+            number += 1
+            num_plus_line = number + 
 
 
+    
+        
+
+print(lines_printed_backwards(poems))
+
+# lines = inline.readlines()
+# print_lines = print(lines.reverse())
+# return print_lines
+
+
+
+
+
+
+# inline = open('poem.txt', 'r')
+# poem = inline.read()
+# print(poem)
+# inline.close()
+
+
+
+
+
+
+# lines_reverse = []
+#     inline = open('poem.txt', 'r')
+#     for line in lines_list:
+#         if line == lines_list[0]:
+#             lines_reverse.append(line)
+#         else:
+#             lines_reverse.insert(0, line)
+#     for line in lines_reverse:
+#         print(line)    
