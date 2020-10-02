04f3ce10993e92960597bffa9a61d4cb38c254dc
Ernest Cheung <ErnestCheung02@gmail.com>
2020-10-01 19:31:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,60 @@
-
+from random import choice
 
 def get_file_lines(filename):
-    inline = open('poem.txt', 'r')
+    # Joi: Instead of passing 'poem.txt', use the param filename
+    inline = open(filename, 'r')
     lines = inline.readlines()
-    poem = inline.read()
-    inline.close()
+    # Joi: You do not need the line below (line 8) 
     return lines
 
-print(get_file_lines('poem.txt'))
-poems = get_file_lines('poem.txt')
 
 def lines_printed_backwards(lines_list):
-    line = len(poems)
-    number = 0
-    for i in poems
-        if number < line:
-            number += 1
-            num_plus_line = number + 
+      # Step 1: Look up how to reverse a list using .reverse()
+      lines_list.reverse()
+      # Step 2: Measure the length of the list
+      line_length = len(lines_list)
 
+      for i in range(line_length):
+        line_number = line_length - i
+        line = lines_list[i]
+        print(f"{line_number} {line} ")
+
+# TODO:
+def lines_printed_random(lines_list):
+
+  # Btw, for this function you do not have to print the line number this time. only for line_printed_backwards you needed the line number.
+ 
+  line_length = len(lines_list)
+
+  for i in range(line_length):
+    line_random = choice(lines_list)
+    print(line_random)
 
     
-        
+# --------------------------------------
+  # Notes to remember things
+#get_file_lines = the raw lines of poem.txt
+#lines_list = the listed version of the poem.txt
+#line_length = the amount of lines there are (as an integer)
+#line_number = the line_length - i in range of line_length
+#
+#
+#
 
-print(lines_printed_backwards(poems))
 
+
+# --------------------------------------
+# TODO:
+# def lines_printed_custom(lines_list):
+
+
+poems = get_file_lines('poem.txt') 
+
+lines_printed_backwards(poems)
+lines_printed_random(poems)
+# lines_printed_custom(poems)
+
+#  ^^^ Uncomment each line after your finished you defining the function.
 # lines = inline.readlines()
 # print_lines = print(lines.reverse())
 # return print_lines
