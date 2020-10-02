e01ca3ac0721294e5523d9c9762ad519ea209eb7
Ernest Cheung <ErnestCheung02@gmail.com>
2020-10-02 13:29:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from random import choice
 
 def get_file_lines(filename):
-    # Joi: Instead of passing 'poem.txt', use the param filename
+    
     inline = open(filename, 'r')
     lines = inline.readlines()
-    # Joi: You do not need the line below (line 8) 
+    
     return lines
 
 
+
+
+
 def lines_printed_backwards(lines_list):
-      # Step 1: Look up how to reverse a list using .reverse()
+      
       lines_list.reverse()
-      # Step 2: Measure the length of the list
+      
       line_length = len(lines_list)
 
       for i in range(line_length):
@@ -19,20 +22,32 @@ def lines_printed_backwards(lines_list):
         line = lines_list[i]
         print(f"{line_number} {line} ")
 
-# TODO:
+
+
+
+
 def lines_printed_random(lines_list):
-
-  # Btw, for this function you do not have to print the line number this time. only for line_printed_backwards you needed the line number.
  
-  line_length = len(lines_list)
+   line_length = len(lines_list)
 
-  for i in range(line_length):
-    line_random = choice(lines_list)
-    print(line_random)
+   for i in range(line_length):
+     line_random = choice(lines_list)
+     print(line_random)
+
+def lines_printed_custom(lines_list):
+    line_length = len(lines_list)
+    line_first_half = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
+    while str(line_length) in line_first_half:
+        print(lines_list[1:8])
+    else:
+        lines_reversed = lines_list.reverse()
+        print(lines_reversed)
+
 
     
 # --------------------------------------
   # Notes to remember things
+#
 #get_file_lines = the raw lines of poem.txt
 #lines_list = the listed version of the poem.txt
 #line_length = the amount of lines there are (as an integer)
@@ -44,15 +59,20 @@ def lines_printed_random(lines_list):
 
 
 # --------------------------------------
-# TODO:
-# def lines_printed_custom(lines_list):
-
 
 poems = get_file_lines('poem.txt') 
 
 lines_printed_backwards(poems)
 lines_printed_random(poems)
-# lines_printed_custom(poems)
+lines_printed_custom(poems)
+
+
+
+
+
+
+
+
 
 #  ^^^ Uncomment each line after your finished you defining the function.
 # lines = inline.readlines()
@@ -63,23 +83,3 @@ lines_printed_random(poems)
 
 
 
-
-# inline = open('poem.txt', 'r')
-# poem = inline.read()
-# print(poem)
-# inline.close()
-
-
-
-
-
-
-# lines_reverse = []
-#     inline = open('poem.txt', 'r')
-#     for line in lines_list:
-#         if line == lines_list[0]:
-#             lines_reverse.append(line)
-#         else:
-#             lines_reverse.insert(0, line)
-#     for line in lines_reverse:
-#         print(line)    
